6a8daeea4e3d1646f0dc1254900bfc425026f63f
Stephen Ochieng <materiamystic@gmail.com>
2024-04-13 18:55:03 +0300

    Adds payments POST and history feature

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const businessForm = document.querySelector("#business_form");
   const payForm = document.querySelector("#pay_form");
+  const history = document.querySelector("#historyItems");
   const dbUrl = "http://localhost:3000";
 
   businessForm.addEventListener("submit", (e) => {
@@ -9,11 +10,20 @@ document.addEventListener("DOMContentLoaded", () => {
     let accountNumInput = e.target.account_num.value;
     let businessName = e.target.business_name.value;
 
-    handlePost(businessNumInput, accountNumInput, businessName);
+    handlePaybillPost(businessNumInput, accountNumInput, businessName);
+  });
+
+  payForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const businessPayInput = e.target.business_numPay.value;
+    const accountPayInput = e.target.account_numPay.value;
+    const payAmount = parseInt(e.target.amount.value);
+
+    handlePaymentPost(businessPayInput, accountPayInput, payAmount);
   });
 
   //POST function
-  function handlePost(businessNum, accountNum, name) {
+  function handlePaybillPost(businessNum, accountNum, name) {
     fetch(`${dbUrl}/paybill`, {
       method: "POST",
       headers: {
@@ -27,6 +37,35 @@ document.addEventListener("DOMContentLoaded", () => {
     })
       .then((res) => res.json())
       .then((body) => console.log(body))
+      .catch((error) => alert(error.message));
+  }
+
+  function handlePaymentPost(businessNum, accountNum, amount) {
+    fetch(`${dbUrl}/payments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        businessNumber: businessNum,
+        accountNumber: accountNum,
+        amountPaid: amount,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+        ---------- ---------- -----------
+        <p>Business Number: <span>${data.businessNumber}</span></p>
+        <p>Account Number: <span>${data.accountNumber}</span></p>
+        <p>Amount: <span>${data.amountPaid}</span></p>
+        <hr>
+        `;
+
+        history.appendChild(li);
+      })
       .catch((error) => alert(error.message));
   }
 });
